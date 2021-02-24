2788c0b36a0f310513b1c098185cc09e039a877d
Jalal uddin Qureshi <58306478+jqureshi0001@users.noreply.github.com>
2021-02-23 19:05:00 -0500

    Add files via upload

---
+++ DoctorServicesReview/indexDoctor.php
@@ -0,0 +1,7 @@
+<?php
+
+include 'header.php';
+include 'doctorBody.php';
+include 'footer.php';
+
+?>
---
+++ DoctorServicesReview/indexServices.php
@@ -0,0 +1,7 @@
+<?php
+
+include 'header.php';
+include 'servicesBody.php';
+include 'footer.php';
+
+?>
