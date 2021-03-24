1b6c81cf2a9b311c1b33a1a9a99c7740c91e057a
Braydon Viragh <bradyviragh@gmail.com>
2021-03-24 15:12:13 -0400

    Created Database class to connect to MySql database + added autoload classes to json

---
+++ classes/Database.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace PHP\classes;
+
+class Database {
+
+    private static $user = "root";
+    private static $pass = "";
+    private static $dsn ="mysql:host=localhost;dbname=qc_health_clinic";
+    private static $dbcon;
+
+    private function __construct() {
+
+    }
+
+    //get pdo connection
+    public static function getDb(){
+        if(!isset(self::$dbcon)) {
+            try {
+                self::$dbcon = new \PDO(self::$dsn, self::$user, self::$pass);
+                self::$dbcon->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+                self::$dbcon->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
+            } catch (\PDOException $e) {
+                $msg = $e->getMessage();
+                include '../custom-error.php';
+                exit();
+            }
+        }
+
+        return self::$dbcon;
+    }
+
+}
