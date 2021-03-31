f4aac34fc6655b5f2144aa42807bec17d39c14aa
franck cheuzem <franckcheuzem@MacBook-Pro-de-franck.local>
2021-03-31 12:40:37 -0400

    reviewed header code

--- pages/header.php
+++ pages/header.php
@@ -9,7 +9,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent-4">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item ">
-                            <a class="nav-link <?php echo ($_GET['page']==='home')? 'active' : '' ?>" href="index.php?page=home">Home </a> 
+                            <a class="nav-link <?php echo ($_GET['page']==='home' || !isset($_GET['page']))? 'active' : '' ?>" href="index.php?page=home">Home </a> 
                         </li>
                         <li class="nav-item ">
                             <a class="nav-link  <?php echo ($_GET['page']==='about_us')? 'active' : '' ?>" href="index.php?page=about_us"> About us </a>
