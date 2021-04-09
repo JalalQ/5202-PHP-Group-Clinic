ba8868e35b6beaffcecc467192f39522c6b1da56
Braydon Viragh <bradyviragh@gmail.com>
2021-04-09 17:39:29 -0400

    Braydon Viragh: Adding pages to flesh out my features

--- pages/deletedUser.php
+++ pages/deletedUser.php
@@ -22,13 +22,14 @@
             <div class="col-md-12">
                 <div class="row " >
                     <div class="col text-center">
-                        <h1 class="py-3">User Added</h1>
+                        <h1 class="py-3">User Deleted</h1>
                         <a class="btn btn-secondary" href="#">Return</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</section>
 <!-- jQuery -->
 <script src="js/jquery-3.5.1.min.js"></script>
 <!-- Bootstrap 4.5 JS -->
