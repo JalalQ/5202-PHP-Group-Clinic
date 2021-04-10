69e9e6fc86faae50829f3e023c5a846ef7f2d90c
Jalaluddin Qureshi <jala0001@e.ntu.edu.sg>
2021-04-09 21:15:00 -0400

    New Services added on Service Page

--- pages/servicesBody.php
+++ pages/servicesBody.php
@@ -6,7 +6,7 @@
         <div class="container">
           <h1 class="jumbotron-heading">Services offered</h1>
           <p class="lead text-muted">
-              The following are the main services offered at our centre.
+              The following are the list services offered at our medical centre.
           </p>
         </div>
       </section>
@@ -15,12 +15,13 @@
         <div class="container">
 
           <div class="row">
+
             <div class="col-md-4">
               <div class="card mb-4 box-shadow">
-                <img class="card-img-top" src="img/services/chiro.jpg" alt="Complaints, Feedback and Compliment" > <!--style="width:200px;height:200px;"-->
+                <img class="card-img-top" src="img/services/chiro.jpg" alt="Chiropractic" >
                 <div class="card-body">
                     <div class="d-flex justify-content-between align-items-center">
-                        <a href="#" class="btn btn-primary my-2">Chiropractic</a>
+                        <p class="btn btn-primary my-2" style="cursor:default;">Chiropractic</p>
                     </div>
                     <p class="card-text">
                         Chiropractic care is a patient-centred, hands-on discipline that focuses on the spine,
@@ -31,12 +32,13 @@
                 </div>
               </div>
             </div>
+
               <div class="col-md-4">
                   <div class="card mb-4 box-shadow">
-                      <img class="card-img-top" src="img/services/spiritual.jpg" alt="Spiritual Care" > <!--style="width:200px;height:200px;"-->
+                      <img class="card-img-top" src="img/services/spiritual.jpg" alt="Physiotherapy" >
                       <div class="card-body">
                           <div class="d-flex justify-content-between align-items-center">
-                              <a href="#" class="btn btn-primary my-2">Physiotherapy</a>
+                              <p class="btn btn-primary my-2" style="cursor:default;">Physiotherapy</p>
                           </div>
                           <p class="card-text">
                               Physiotherapists help people affected by injury, illness or disability through
@@ -47,12 +49,13 @@
                       </div>
                   </div>
               </div>
+
               <div class="col-md-4">
                   <div class="card mb-4 box-shadow">
-                      <img class="card-img-top" src="img/services/acu.jpg" alt="Interpreter Translation" > <!--style="width:200px;height:200px;"-->
+                      <img class="card-img-top" src="img/services/acu.jpg" alt="Acupuncture" >
                       <div class="card-body">
                           <div class="d-flex justify-content-between align-items-center">
-                              <a href="#" class="btn btn-primary my-2">Acupuncture</a>
+                              <p class="btn btn-primary my-2" style="cursor:default;">Acupuncture</p>
                           </div>
                           <p class="card-text">
                               Acupuncture is a nerve stimulation technique that uses fine needles and
@@ -64,7 +67,55 @@
                       </div>
                   </div>
               </div>
+
           </div>
+
+
+            <div class="row">
+
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img class="card-img-top" src="img/services/occupation.jpg" alt="Occupational Therapy" >
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <p class="btn btn-primary my-2" style="cursor:default;">
+                                    Occupational Therapy
+                                </p>
+                            </div>
+                            <p class="card-text">
+                                Occupational therapy is a client-centred health profession concerned with promoting
+                                health and well being through occupation. The primary goal of occupational therapy is
+                                to enable people to participate in the activities of everyday life.
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img class="card-img-top" src="img/services/massage.jpg" alt="Massage Therapy" >
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <p class="btn btn-primary my-2" style="cursor:default;">
+                                    Massage Therapy
+                                </p>
+                            </div>
+                            <p class="card-text">
+                                Massage Therapy is the manipulation of the body's soft tissues. Massage
+                                techniques are commonly applied with hands, fingers, elbows, knees, forearms,
+                                feet, or a device. The purpose of massage is generally for the treatment of body
+                                stress or pain.
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
 
 
 
